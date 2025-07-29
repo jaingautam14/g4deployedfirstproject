9974aeda3cd5bee7b13c99f9595ec4fedbf761f8
Gautam <jaingautamcse23@gmail.com>
2025-07-30 03:10:49 +0530

    first commit

---
+++ api/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const ejs = require("ejs");
+
+app.set("view engine","ejs");
+
+app.use(express.static("public"));
+app.set("views", path.join(__dirname,'../views'));
+
+app.get('/',(req, res) => {
+    res.render("home");
+})
+
+module.exports = app;
+
+app.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
